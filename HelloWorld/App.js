6504dbb66675538c350bca1ab91008cb8c5a2677
Laura Joy Erb <laurajoy@bu.edu>
2020-04-11 19:41:33 -0400

    using props

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
-function HelloWorldApp() {
+const styles = StyleSheet.create({
+  center: {
+    alignItems: 'center'
+  }
+})
+
+// defines a component
+// we can pass variables in for props
+function Greeting(props) {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-      <Text>Hello, world!</Text>
+    <View style={styles.center}>
+      <Text>Hello {props.name}!</Text>
     </View>
-  )
+  );
 }
-export default HelloWorldApp;
+
+// this function determines the view, and uses three Greeting components
+function LotsOfGreetings() {
+  return (
+    <View style={[styles.center, { top: 50 }]}>
+      <Greeting name='Rexxar' />
+      <Greeting name='Jaina' />
+      <Greeting name='Valeera' />
+    </View>
+  );
+}
+
+export default LotsOfGreetings;
